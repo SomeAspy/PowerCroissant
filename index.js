c71cb9c6303bb100a66bdb3eb0ee00da7c3155fc
Aiden Baker <33640860+SomeAspy@users.noreply.github.com>
2020-12-23 21:40:46 -0500

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const { Plugin } = require('powercord/entities');
 module.exports = class Mock extends Plugin {
   startPlugin () {
         document.addEventListener("click",         function play() {
-            var audio = new Audio('https://raw.githubusercontent.com/SomeAspy/croissant/main/croissant%20sound.mp3');
+            var audio = new Audio('https://raw.githubusercontent.com/SomeAspy/PowerCroissant/main/croissant%20sound.mp3');
             audio.play();
           })
   }
