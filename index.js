cc8fb123aa92d0400ac7e27013d88d8c3930ed58
Aiden Baker <33640860+CaptainSnowball@users.noreply.github.com>
2020-12-23 21:39:21 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
+  
+const { Plugin } = require('powercord/entities');
 
+module.exports = class Mock extends Plugin {
+  startPlugin () {
+        document.addEventListener("click",         function play() {
+            var audio = new Audio('https://raw.githubusercontent.com/SomeAspy/croissant/main/croissant%20sound.mp3');
+            audio.play();
+          })
+  }
+
+  pluginWillUnload () {
+    powercord.api.commands.unregisterCommand('mock');
+  }
+};
