b57338035e36deba6b6477e382f7d1069d6de297
Aiden Baker <33640860+SomeAspy@users.noreply.github.com>
2020-12-23 21:45:23 -0500

    Update index.js

--- index.js
+++ index.js
@@ -10,5 +10,9 @@ module.exports = class PowerCroissant extends Plugin {
   }
 
   pluginWillUnload () {
+    document.removeEventListener("click",function play() {
+            var audio = new Audio('https://raw.githubusercontent.com/SomeAspy/PowerCroissant/main/croissant%20sound.mp3');
+            audio.play();
+          }))
   }
 };
